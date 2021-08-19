89dc11dfe51e766ff572d01ef0711e43e907fd53
MahesaDS2408 <mahesadarmasatria@gmail.com>
2021-08-19 12:55:10 +0700

    golang enginer

---
+++ Golang Engineer/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/providers/google"
+)
+
+const (
+	key = "101667299166-h2pqpe8gkt84eaoknbq228er0uf91li4.apps.googleusercontent.com"
+	sec = "rH2dauqgONNKM6feTSjt_3z2"
+)
+
+func main() {
+	goth.UseProviders(
+		google.New(key, sec, "http://localhost:3000/auth/google/callback"),
+	)
+
+	app := fiber.New()
+
+	app.Get("/auth/:provider/callback", func(ctx *fiber.Ctx) error {
+		user, err := gf.CompleteUserAuth(ctx)
+		if err != nil {
+			return err
+		}
+		ctx.JSON(user)
+		return nil
+	})
+
+	app.Get("/logout/:provider", func(ctx *fiber.Ctx) error {
+		gf.Logout(ctx)
+		ctx.Redirect("/")
+		return nil
+	})
+
+	app.Get("/auth/:provider", func(ctx *fiber.Ctx) error {
+		if gothUser, err := gf.CompleteUserAuth(ctx); err == nil {
+			ctx.JSON(gothUser)
+		} else {
+			gf.BeginAuthHandler(ctx)
+		}
+		return nil
+	})
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		ctx.Format("<p><a href='/auth/google'>google</a></p>")
+		return nil
+	})
+
+	log.Fatal(app.Listen(":3000"))
+
+}
--- logic test/NearestFibonacci.go
+++ logic test/NearestFibonacci.go
@@ -1,37 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
-func FibonacciLoop(n int) int {
-	f := make([]int, n+1, n+2)
-	if n < 2 {
-		f = f[0:2]
-	}
-	f[0] = 0
-	f[1] = 1
-	for i := 2; i <= n; i++ {
-		f[i] = f[i-1] + f[i-2]
-	}
-	return f[n]
-}
-
-func FibonacciRecursion(n int) int {
+func fib(n int) int {
 	if n <= 1 {
 		return n
 	}
-	return FibonacciRecursion(n-1) + FibonacciRecursion(n-2)
+	return fib(n-1) + fib(n-2)
 }
-
 func main() {
-	for i := 0; i <= 9; i++ {
-		fmt.Print(strconv.Itoa(FibonacciLoop(i)) + " ")
+	var n int
+	fmt.Print("input: ")
+	fmt.Scan(&n)
+
+	fib(n)
+	if n >= 0 {
+		fmt.Println(fib(n))
 	}
-	fmt.Println("")
-	for i := 0; i <= 9; i++ {
-		fmt.Print(strconv.Itoa(FibonacciRecursion(i)) + " ")
-	}
-	fmt.Println("")
+
 }
